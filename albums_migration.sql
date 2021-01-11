e59b52063302d6472f789d26adff3ce36a1ce3bf
John <johntbrooks92@gmail.com>
2021-01-11 14:07:14 -0600

    exercise finished

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       artist VARCHAR(50) NOT NULL DEFAULT 'Unknown',
+                       name VARCHAR(50) NOT NULL DEFAULT 'Untitled',
+                       release_date INT UNSIGNED,
+                       sales FLOAT UNSIGNED,
+                       genre VARCHAR(100),
+                       PRIMARY KEY(id)
+);
